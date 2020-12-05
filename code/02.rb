d35090984b2fef4d7006c32653889ae605e6b04e
Malaber <git@schaedler.rocks>
2020-12-05 02:00:12 +0100

    Add solution to 02b

--- code/02.rb
+++ code/02.rb
@@ -2,6 +2,7 @@ require_relative '../input'
 lines = get_lines $PROGRAM_NAME
 
 counter = 0
+second_counter = 0
 
 lines.each do |line|
   rule, password = line.split(":")
@@ -16,6 +17,12 @@ lines.each do |line|
   if actual <= max and actual >= min
     counter += 1
   end
+
+  first_letter = password[min-1]
+  second_letter = password[max-1]
+
+  second_counter += 1 if (first_letter+second_letter).count(char) == 1
 end
 
 puts counter
+puts second_counter
