73e24fcefda10c70c3915fb7e67d276f9e989e90
Malaber <git@schaedler.rocks>
2020-12-05 02:28:25 +0100

    Add solution to 03b

--- code/03.rb
+++ code/03.rb
@@ -1,7 +1,7 @@
 require_relative '../input'
 lines = get_lines $PROGRAM_NAME
 
-minsize = lines.size * 4 #3 would probably be enough, but I am generous
+minsize = lines.size * 8 #we need mooooore map
 
 duplication = minsize / lines.first.strip.size
 
@@ -9,12 +9,25 @@ lines.map! do |line|
   line.strip * duplication
 end
 
-x = 0
-counter = 0
+def get_trees(lines, right, down)
+  x = 0
+  y = 0
+  counter = 0
 
-lines.each do |line|
-  counter += 1 if line[x] == "#"
-  x += 3
+  lines.each_with_index do |line, i|
+    y += 1
+    next if i % down == 1
+    counter += 1 if line[x] == "#"
+    x += right
+  end
+
+  return counter
 end
 
-puts counter
+puts a = get_trees(lines, 1, 1)
+puts b = get_trees(lines, 3, 1)
+puts c = get_trees(lines, 5, 1)
+puts d = get_trees(lines, 7, 1)
+puts e = get_trees(lines, 1, 2)
+
+puts a * b * c * d * e
