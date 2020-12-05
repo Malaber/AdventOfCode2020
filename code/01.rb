24b75bde39b367560e282a212f10415b1e13124f
Malaber <git@schaedler.rocks>
2020-12-05 11:26:20 +0100

    Remove duplicates from log on 01

--- code/01.rb
+++ code/01.rb
@@ -3,21 +3,29 @@ lines = get_lines $PROGRAM_NAME
 
 lines.map!(&:to_i)
 
-lines.each do |line|
-  lines.each do |line2|
-    if line + line2 == 2020
-      puts line * line2
-    end
-  end
-end
-
-
-lines.each do |line|
-  lines.each do |line2|
-    lines.each do |line3|
-      if line + line2 + line3 == 2020
-        puts line * line2 * line3
+def first(lines)
+  lines.each do |line|
+    lines.each do |line2|
+      if line + line2 == 2020
+        return line * line2
       end
     end
   end
 end
+
+puts first(lines)
+
+
+def second(lines)
+  lines.each do |line|
+    lines.each do |line2|
+      lines.each do |line3|
+        if line + line2 + line3 == 2020
+          return line * line2 * line3
+        end
+      end
+    end
+  end
+end
+
+puts second(lines)
