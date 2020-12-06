4f4fe762a688b78ffc727240ec6916ddabc2c5a6
Malaber <git@schaedler.rocks>
2020-12-06 14:31:12 +0100

    Add solution to 06b

--- code/06.rb
+++ code/06.rb
@@ -22,3 +22,31 @@ groups.each do |group|
 end
 
 p counter
+
+part_2_group = nil
+part_2_groups = []
+
+lines.each do |line|
+  if line == ""
+    part_2_groups << part_2_group
+    part_2_group = nil
+    next
+  end
+
+  if part_2_group.nil?
+    part_2_group = line
+  else
+    line_array = line.split("")
+    part_2_group_array = part_2_group.split("")
+
+    part_2_group = (line_array & part_2_group_array).join("")
+  end
+end
+
+p part_2_groups
+counter_2 = 0
+part_2_groups.each do |group|
+  counter_2 += group.length
+end
+
+p counter_2
