16e3ec6830d5a11e02967951a4696721f5ee0113
Malaber <git@schaedler.rocks>
2020-12-07 23:54:51 +0100

    Add solution to 07b

--- code/07.rb
+++ code/07.rb
@@ -37,22 +37,20 @@ p can_contain_golden_bag.map{|b|b[:bag_color]}.uniq.count
 
 p rules
 
-@correction = 0
 def how_many_bags_are_in_the_bag(bag, rules)
-  inside = 0
+  inside = 1
   inside_bags = bag[:inside_bags]
   inside_bags.each do |inside_bag|
     if inside_bag[:amount].nil?
-      inside += 1
-      @correction += 1
+
     else
-      inside += 1 + inside_bag[:amount].to_i *
-          how_many_bags_are_in_the_bag(rules.find{|r|
-            r[:bag_color] == inside_bag[:bag_color]
-          }, rules)
+      inside_bag_as_bag = rules.find{|r|
+        r[:bag_color] == inside_bag[:bag_color]
+      }
+      inside += (inside_bag[:amount].to_i * how_many_bags_are_in_the_bag(inside_bag_as_bag, rules))
     end
   end
   return inside
 end
 
-p how_many_bags_are_in_the_bag(golden_shiny_bag, rules) - @correction
+p how_many_bags_are_in_the_bag(golden_shiny_bag, rules) - 1
