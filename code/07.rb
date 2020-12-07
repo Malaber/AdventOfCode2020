94c4135f47e47493521eb380ffb705d05d7605d5
Malaber <git@schaedler.rocks>
2020-12-07 20:54:47 +0100

    Finish solution to 07a

--- code/07.rb
+++ code/07.rb
@@ -2,6 +2,8 @@ require_relative '../input'
 lines = get_lines $PROGRAM_NAME
 
 rules = []
+golden_shiny_bag = nil
+can_contain_golden_bag = []
 
 lines.each do |line|
   # light red bags contain 1 bright white bag, 2 muted yellow bags.
@@ -11,9 +13,24 @@ lines.each do |line|
   inside_bags = inside_bags.strip.gsub(".", "").split(", ").map{|bag| amount = bag[/\d+/]; {amount: amount, bag_color: bag.gsub(/\d+ /, "").gsub(/ bags/, "").gsub(/ bag/, "")}}
 
   bag = {bag_color: bag_color, inside_bags: inside_bags}
-  bag >> rules
+  rules << bag
+
+  golden_shiny_bag = bag if bag[:bag_color] == "shiny gold"
 end
 
-bagtree = rules.map do |rule|
-
+def contains_golden_bag(bag, rules, can_contain_golden_bag)
+  can_contain_golden_bag << bag unless bag[:bag_color] == "shiny gold"
+  bag_color = bag[:bag_color]
+  rules.each do |rule|
+    rule[:inside_bags].each do |inside_bag|
+      if inside_bag[:bag_color] == bag_color
+        can_contain_golden_bag << rule
+        contains_golden_bag(rule, rules, can_contain_golden_bag)
+      end
+    end
+  end
 end
+
+contains_golden_bag(golden_shiny_bag, rules, can_contain_golden_bag)
+
+p can_contain_golden_bag.map{|b|b[:bag_color]}.uniq.count
