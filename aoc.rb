bfd107d238551fa0baff4129488776f55abef7da
Malaber <git@schaedler.rocks>
2020-12-08 10:25:07 +0100

    Update aco ruby generation script

--- aoc.rb
+++ aoc.rb
@@ -6,5 +6,7 @@ exit 1 if File.exists?("code/#{script_number}.txt")
 File.open("inputs/#{script_number}.txt", "w"){|f| f.write("")}
 File.open("code/#{script_number}.rb", "w"){|f| f.write(
 "require_relative '../input'
+require_relative 'handheld/Instruction'
+require_relative 'handheld/Computer'
 lines = get_lines $PROGRAM_NAME
 ")}
