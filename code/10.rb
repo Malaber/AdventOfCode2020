929f7fd01d788ec8eccacc140af3229a1402a9f0
Malaber <git@schaedler.rocks>
2020-12-10 19:09:21 +0100

    Add "solution" to 10b
    This solution will finish (probably) but I will very likely not have the time and memory required to wait for that to happen....

--- code/10.rb
+++ code/10.rb
@@ -8,6 +8,12 @@ lines.map!(&:to_i).sort!
 CHARGING_OUTLET_VOLTAGE = 0
 @device_joltage = lines.max + 3
 
+class JoltageError < StandardError
+  def initialize(msg="Not enough Joltage")
+    super
+  end
+end
+
 def get_difference_to_next(voltages, index)
   voltage = voltages[index]
   next_voltage = voltages[index + 1]
@@ -24,6 +30,7 @@ def get_difference_array_for_lines(lines)
 
   voltages.each_with_index do |voltage, index|
     difference = get_difference_to_next(voltages, index)
+    raise JoltageError if difference > 3
     dif[difference] += 1
   end
   dif
@@ -32,3 +39,20 @@ end
 dif = get_difference_array_for_lines(lines)
 
 p dif[1] * dif[3]
+
+permutations = []
+p lines.size
+lines.size.times do |i|
+  permutations << lines.permutation(i + 1).to_a
+end
+
+sum = 0
+permutations.flatten(1).each do |permutation|
+  begin
+    get_difference_array_for_lines(permutation)
+    sum += 1
+  rescue
+    JoltageError
+  end
+end
+p sum
