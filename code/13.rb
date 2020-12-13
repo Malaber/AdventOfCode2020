29af31d92703fe1832386ed5c84aec78a1096b35
Malaber <git@schaedler.rocks>
2020-12-13 12:29:34 +0100

    Add solution to 13a

---
+++ code/13.rb
@@ -0,0 +1,22 @@
+require_relative '../input'
+require_relative 'handheld/Instruction'
+require_relative 'handheld/Computer'
+lines = get_lines $PROGRAM_NAME
+
+arrival_time = lines.first.to_i
+buses = lines.last.split(",").select{ |x| x != "x"}.map(&:to_i)
+
+departures = {}
+
+buses.each do |bus|
+  earliest_departure = 0
+  until arrival_time <= earliest_departure
+    earliest_departure += bus
+  end
+
+  departures[bus] = earliest_departure
+end
+
+bus_id, departure_time = departures.min_by{|k,v| v}
+
+puts bus_id * (departure_time - arrival_time)
