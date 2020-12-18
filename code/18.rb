6bdc7c72fc14afa85d446d1c072df70bb62d5ee5
Malaber <git@schaedler.rocks>
2020-12-18 18:51:04 +0100

    Extract math part to method

--- code/18.rb
+++ code/18.rb
@@ -5,16 +5,7 @@ lines = get_lines $PROGRAM_NAME
 
 lines.map!(&:strip)
 
-def solve(line)
-  p line
-  subline_regex = /\(([^()]+?)\)/
-  until line.count("(").zero?
-    subline = line.match(subline_regex).captures.first
-    subline_result = solve(subline)
-    line.sub!(subline_regex, subline_result.to_s)
-  end
-  p line
-
+def do_math(line)
   result = nil
   operator = nil
   line.split(/\s+/).each do |ins|
@@ -44,6 +35,19 @@ def solve(line)
   end
 
   result
+end
+
+def solve(line)
+  p line
+  subline_regex = /\(([^()]+?)\)/
+  until line.count("(").zero?
+    subline = line.match(subline_regex).captures.first
+    subline_result = solve(subline)
+    line.sub!(subline_regex, subline_result.to_s)
+  end
+  p line
+
+  do_math(line)
 end
 
 sum = 0
